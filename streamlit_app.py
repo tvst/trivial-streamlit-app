556eb1f8080cd909b9ba0516521ec4c9d93db43f
Thiago Teixeira <thiagot@gmail.com>
2020-12-28 15:41:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ This is the most basic Streamlit app _ever_!
 
 v = st.slider("Choose a value", 0, 100)
 
-"You selected:", v, "!!!"
+"You selected:", v, "!!!!"
 
 # Also print to console.
 print(f"You selected: {v}")
